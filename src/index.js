fbcfe61266b3c5f4e63282ea34f8953abd57622a
Bimbalyot <reznikartem2000@gmail.com>
2025-06-06 15:01:29 +0200

    Create webpage

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import "./scss/reset.scss";
+import "./sass/styles.sass";
+
+import "./assets/img/Logo.svg";
+import "./assets/img/Clip path group.svg";
+import "./assets/img/favicon.png";
+import "./assets/img/trezub.svg";
+import "./assets/img/fact1.svg";
+import "./assets/img/fact2.svg";
+import "./assets/img/fact3.svg";
